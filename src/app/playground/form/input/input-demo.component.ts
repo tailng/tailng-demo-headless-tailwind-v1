bfdbc3217814828fb6b663afb9eaf9e7ab8aa4d9
Prince Francis <princekfrancis@gmail.com>
2026-03-26 19:04:09 +0530

    Input demo added.

--- src/app/playground/form/input/input-demo.component.ts
+++ src/app/playground/form/input/input-demo.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TngInput, TngInputGroup, TngPrefix, TngSuffix } from '@tailng-ui/primitives';
 
 @Component({
   selector: 'app-input-demo',
   standalone: true,
   templateUrl: './input-demo.component.html',
+  imports: [TngInput, TngInputGroup, TngPrefix, TngSuffix],
 })
 export class InputDemoComponent {}
