c91f4202449a9ef582062e0d8d696d928ca54d71
Prince Francis <princekfrancis@gmail.com>
2026-03-27 15:45:38 +0530

    example added for input api section.

--- src/app/playground/form/input/input-demo.component.ts
+++ src/app/playground/form/input/input-demo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TngInput, TngInputGroup, TngPrefix, TngSuffix } from '@tailng-ui/primitives';
+import { TngIcon } from '@tailng-ui/icons';
 
 @Component({
   selector: 'app-input-demo',
   standalone: true,
   templateUrl: './input-demo.component.html',
-  imports: [TngInput, TngInputGroup, TngPrefix, TngSuffix],
+  imports: [TngInput, TngInputGroup, TngPrefix, TngSuffix, TngIcon],
 })
 export class InputDemoComponent {}
