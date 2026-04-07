ed24066918848c98fba5eeedbf17ad0b134edd82
Prince Francis <princekfrancis@gmail.com>
2026-04-07 12:52:47 +0530

    combobox removed.

--- src/app/playground/form/combobox/combobox.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-combobox',
-  standalone: true,
-  templateUrl: './combobox.component.html',
-})
-export class ComboboxComponent {}
